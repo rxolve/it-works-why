fddebb6bfb1bdd80992f26e2ab60eedb656aac16
rxolve <rxolve@gmail.com>
2023-11-01 15:54:12 +0900

    feat: add count-div

--- src/routes/algorithm/+page.svelte
+++ src/routes/algorithm/+page.svelte
@@ -1,7 +1,8 @@
 <script>
-	// import solve
 	import { demicalToBinary } from '../../solve/demical-to-binary';
+	import { countDiv } from '../../solve/count-div';
 </script>
 
 <h1>Algorithm</h1>
 <p>demicalToBinary: {demicalToBinary(1024)}</p>
+<p>countDiv: {countDiv(11, 345, 17)}</p>
