3fef468514f0e8f90ae5055c70a50a4b5cd1bacc
rxolve <rxolve@gmail.com>
2023-11-01 16:24:13 +0900

    feat: add number-of-disc-intersections

--- src/routes/algorithm/+page.svelte
+++ src/routes/algorithm/+page.svelte
@@ -1,8 +1,10 @@
 <script>
 	import { demicalToBinary } from '../../solve/demical-to-binary';
 	import { countDiv } from '../../solve/count-div';
+	import { numberOfDiscIntersections } from '../../solve/number-of-disc-intersections';
 </script>
 
 <h1>Algorithm</h1>
 <p>demicalToBinary: {demicalToBinary(1024)}</p>
 <p>countDiv: {countDiv(11, 345, 17)}</p>
+<p>numberOfDiscIntersections: {numberOfDiscIntersections([1, 1, 1])}</p>
