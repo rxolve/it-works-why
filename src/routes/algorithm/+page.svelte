c3be534cd399b3f9f2fe2b35d591ebf48ec23953
rxolve <rxolve@gmail.com>
2023-11-02 11:12:06 +0900

    feat: add binary search tree

--- src/routes/algorithm/+page.svelte
+++ src/routes/algorithm/+page.svelte
@@ -2,9 +2,11 @@
 	import { demicalToBinary } from '../../solve/demical-to-binary';
 	import { countDiv } from '../../solve/count-div';
 	import { numberOfDiscIntersections } from '../../solve/number-of-disc-intersections';
+	import { binarySearchTree } from '../../solve/binary-search-tree';
 </script>
 
 <h1>Algorithm</h1>
 <p>demicalToBinary: {demicalToBinary(1024)}</p>
 <p>countDiv: {countDiv(11, 345, 17)}</p>
 <p>numberOfDiscIntersections: {numberOfDiscIntersections([1, 1, 1])}</p>
+<p>binarySearchTree: {binarySearchTree([4, 2, 6, 1, 3, 5, 7])}</p>
