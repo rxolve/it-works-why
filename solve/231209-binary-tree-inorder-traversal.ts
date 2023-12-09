364e288470087aa59f5d42df0b4c062c9300a6ff
rxolve <rxolve@gmail.com>
2023-12-09 13:33:50 +0900

    solve: 231209-binary-tree-inorder-traversal

---
+++ solve/231209-binary-tree-inorder-traversal.ts
@@ -0,0 +1,29 @@
+/*
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+*/
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function inorderTraversal(root: TreeNode | null): number[] {
+  const result: number[] = [];
+  const stack: TreeNode[] = [];
+  let current = root;
+  while (current || stack.length) {
+    while (current) {
+      stack.push(current);
+      current = current.left;
+    }
+    current = stack.pop()!;
+    result.push(current.val);
+    current = current.right;
+  }
+  return result;
+}
