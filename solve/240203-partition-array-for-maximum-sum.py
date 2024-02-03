abbd11a96794c0d6f1273ca71dca47951a856015
help <rxolve@gmail.com>
2024-02-03 14:31:58 +0900

    solve: 240203-partition-array-for-maximum-sum

---
+++ solve/240203-partition-array-for-maximum-sum.py
@@ -0,0 +1,20 @@
+"""
+Given an integer array arr, partition the array into (contiguous) subarrays of length at most k. After partitioning, each subarray has their values changed to become the maximum value of that subarray.
+
+Return the largest sum of the given array after partitioning. Test cases are generated so that the answer fits in a 32-bit integer.
+"""
+
+class Solution(object):
+    def maxSumAfterPartitioning(self, arr, k):
+        """
+        :type arr: List[int]
+        :type k: int
+        :rtype: int
+        """
+        dp = [0] * (len(arr) + 1)
+        for i in range(1, len(arr) + 1):
+            max_val = 0
+            for j in range(1, min(i, k) + 1):
+                max_val = max(max_val, arr[i - j])
+                dp[i] = max(dp[i], dp[i - j] + max_val * j)
+        return dp[len(arr)]
