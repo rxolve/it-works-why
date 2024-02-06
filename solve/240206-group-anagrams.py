16c1326c374e4a9609be268a34166c064f3cc226
help <rxolve@gmail.com>
2024-02-06 11:32:00 +0900

    solve: 240206-group-anagrams

---
+++ solve/240206-group-anagrams.py
@@ -0,0 +1,18 @@
+"""
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+"""
+
+from collections import defaultdict
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        anagrams = defaultdict(list)
+        for s in strs:
+            anagrams["".join(sorted(s))].append(s)
+        return list(anagrams.values())
